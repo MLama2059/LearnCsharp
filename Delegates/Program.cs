b96cad93caafb9bdba8867ef7de905423cb7fcb1
MLama2059 <lamamanish2059@gmail.com>
2026-04-03 14:55:35 +0545

    Practice using Delegates in C#

---
+++ Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Delegate
+{
+    public delegate void CallBack(string message);
+    public delegate int CallBack1(int a, int b);
+
+    public static void DelegateMethod(string message)
+    {
+        Console.WriteLine(message);
+    }
+
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static void Main(string[] args)
+    {
+        CallBack callBack = DelegateMethod;
+        callBack("Hello world!");
+
+        Delegate del = new Delegate();
+        CallBack1 callBack1 = del.Add;
+        Console.WriteLine(callBack1(1, 2));
+    }
+}
