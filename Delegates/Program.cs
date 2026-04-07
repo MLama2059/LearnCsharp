29bae3e91d44aff0f17af33dd4a50aa6481a5284
MLama2059 <lamamanish2059@gmail.com>
2026-04-07 09:09:51 +0545

    Practice of Extension Methods in C#

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -21,5 +21,17 @@
         Delegate del = new Delegate();
         CallBack1 callBack1 = del.Add;
         Console.WriteLine(callBack1(1, 2));
+
+        // Using Action delegate
+        Action<string> printName = name => Console.WriteLine(name);
+        printName("Manish Lama");
+
+        // Using Func delegate
+        Func<string, int, string> nameAndAge = (name, age) => $"{name} is {age} years old.";
+        Console.WriteLine(nameAndAge("Manish", 23));
+
+        // Using Predicate delegate
+        Predicate<int> isYoung = age => age < 30;
+        Console.WriteLine($"Is young: {isYoung(23)}");
     }
 }
---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿string message = "Hello, world!";
+message.Print();
+Console.WriteLine($"{message} has {message.WordCount()} characters.");
+
+Car car = new Car();
+car.Model("Tesla");
+car.Price(100000);
+
+public class Car
+{
+    public void Model(string model)
+    {
+        Console.WriteLine(model);
+    }
+}
+public static class MyExtensions
+{
+    public static void Print(this string str)
+    {
+        Console.WriteLine(str);
+    }
+
+    public static int WordCount(this string str)
+    {
+        char[] letters = str.ToCharArray();
+        int count = 0;
+        foreach (char letter in letters)
+        {
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void Price(this Car car, int price)
+    {
+        Console.WriteLine(price);
+    }
+}
